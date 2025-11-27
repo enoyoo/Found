66494f2358bf137f439ab7ab9f41a84d1cb3b497
Eno <enoyoo02@gmail.com>
2025-11-26 22:14:18 -0500

    Initial Commit

---
+++ Found/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Found
+//
+//  Created by Eno Yoo on 11/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Found/FoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundApp.swift
+//  Found
+//
+//  Created by Eno Yoo on 11/26/25.
+//
+
+import SwiftUI
+
+@main
+struct FoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
